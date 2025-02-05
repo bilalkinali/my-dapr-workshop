42783e4559d355879074281d90fbddd92248f74f
Bilal Kinali <biki59986@edu.ucl.dk>
2025-02-05 22:40:47 +0100

    added dapr

---
+++ start-here/DaprWorkshop.AppHost/Program.cs
@@ -0,0 +1,48 @@
+using Aspire.Hosting.Dapr;
+using Projects;
+
+var builder = DistributedApplication.CreateBuilder(args);
+
+var statestore = builder.AddDaprStateStore("pizzastatestore");
+
+builder.AddProject<PizzaOrder>("pizzaorderservice")
+    .WithDaprSidecar(new DaprSidecarOptions
+    {
+        AppId = "pizza-order",
+        DaprHttpPort = 3501
+    })
+    .WithReference(statestore);
+
+builder.AddProject<PizzaStorefront>("pizzastorefrontservice")
+    .WithDaprSidecar(new DaprSidecarOptions
+    {
+        AppId = "pizza-storefront",
+        DaprHttpPort = 3502
+    })
+    .WithReference(statestore);
+
+builder.AddProject<PizzaKitchen>("pizzakitchenservice")
+    .WithDaprSidecar(new DaprSidecarOptions
+    {
+        AppId = "pizza-kitchen",
+        DaprHttpPort = 3503
+    })
+    .WithReference(statestore);
+
+builder.AddProject<PizzaDelivery>("pizzadeliveryservice")
+    .WithDaprSidecar(new DaprSidecarOptions
+    {
+        AppId = "pizza-delivery",
+        DaprHttpPort = 3504
+    })
+    .WithReference(statestore);
+
+//builder.AddProject<Projects.PizzaOrder>("pizzaorder");
+
+//builder.AddProject<Projects.PizzaStorefront>("pizzastorefront");
+
+//builder.AddProject<Projects.PizzaKitchen>("pizzakitchen");
+
+//builder.AddProject<Projects.PizzaDelivery>("pizzadelivery");
+
+builder.Build().Run();
